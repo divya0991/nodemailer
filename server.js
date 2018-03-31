51e3857a437c582a1004309bf4cd8dd3edf78a05
divya0991 <divya.babbr0991@gmail.com>
2018-03-31 14:44:22 +0530

    this is nodemailer code

---
+++ server.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var bodyparser = require('body-parser');
+var nodemailer = require('nodemailer');
+var smtpTransport = require('nodemailer-smtp-transport');
+var app = express();
+var cors = require('cors');
+var path = require('path');
+var port = process.env.PORT || 8000;
+app.use(cors())
+var transporter = nodemailer.createTransport(smtpTransport({
+  service: 'gmail',
+  host: 'smtp.gmail.com',
+  auth: {
+    user: 'ss4u.team.node@gmail.com',
+    pass: 'Marchend'
+  }
+}));
+app.use(bodyparser.json());
+app.use(express.static(path.join(__dirname,"public")));
+
+app.post("/email",function(req,res)
+    {
+        var mailOptions = 
+        {
+         // sender address
+          to: req.body.to, // list of receivers
+          subject:req.body.subject, // Subject line
+          text: req.body.message// plain text body
+        };
+        transporter.sendMail(mailOptions, function (err, info) 
+        {
+        if (err) 
+          {
+            console.log(err);
+              console.log(req.body);
+            res.statuscode = 200;
+            res.write(err);
+            res.end();
+          } else
+          {
+              console.log(req.body);
+            res.statuscode = 200;
+            res.write("hello");
+            res.end();
+            console.log('Email sent: ' + info.response);
+          }                  
+});
+});
+app.listen(port);
